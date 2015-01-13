32872efdd611f58a57b9797377a1ee5ab4efef82
Rosen <karadinev@gmail.com>
2015-01-13 21:05:50 +0200

    adding taskView.xhtml and merge taskMB and CreateTaskMB

--- src/com/tusofia/taskmanager/mb/CreateTaskMB.java
+++ src/com/tusofia/taskmanager/mb/CreateTaskMB.java
@@ -2,10 +2,13 @@ package com.tusofia.taskmanager.mb;
  
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
  
+import javax.faces.event.ActionEvent;
+
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
  
@@ -15,7 +18,8 @@ public class CreateTaskMB {
     private Date dueDate;
 	private String status;
     private String assignee;
-    private String create;
+	private String taskName;
+    private String description;
      
     public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
@@ -29,7 +33,15 @@ public class CreateTaskMB {
         requestContext.update("form:display");
         requestContext.execute("PF('dlg').show()");
     }
- 
+    
+   public void buttonAction(ActionEvent actionEvent) {
+       addMessage("Submited!!");
+   }
+    
+   public void addMessage(String summary) {
+       FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+       FacesContext.getCurrentInstance().addMessage(null, message);
+   }
     public Date getDueDate() {
         return dueDate;
     }
@@ -52,5 +64,19 @@ public class CreateTaskMB {
 	public void setAssignee(String assignee) {
 		this.assignee = assignee;
 	}
+    public String getTaskName() {
+		return taskName;
+	}
 
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
